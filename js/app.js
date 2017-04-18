044ac6004146427e2dfa2c816899a3adf0286043
Nickolas Teixeira <NickolasTeixeira01@gmail.com>
2017-04-18 13:37:19 -0700

    mouse hover functionality for tic tac toe game

--- js/app.js
+++ js/app.js
@@ -61,15 +61,55 @@
     player1.classList.add("active");
   });
 
-  let box1 = document.getElementById("box1");
 
-  box1.addEventListener("mouseover", () => {
-    box1.style.backgroundImage = "url(../img/x.svg)";
-  });
+const boxes = document.querySelector(".boxes").children;
 
-  var game = {
-
+function addingColor(box) {
+  if (box.classList === "box") {
+    return;
+  } else {
+    box.addEventListener("mouseover", function() {
+      box.classList.add("box-filled-1-5");
+    });
   }
+}
+
+function removingColor(box) {
+    box.addEventListener("mouseout", function() {
+      box.classList.remove("box-filled-1-5");
+    });
+}
+
+function changingBackground(box) {
+  box.addEventListener("click", function() {
+    box.classList.remove("box-filled-1-5");
+    box.classList.add("box-filled-1");
+  });
+}
+
+// Giving all the boxes on the game board the ability to add color and remove the color
+for (var i = 0; i < boxes.length; i++) {
+  addingColor(boxes[i]);
+  removingColor(boxes[i]);
+  changingBackground(boxes[i]);
+}
+
+
+
+
+
+
+//Clicking on the box to add the svg
+  // box1.addEventListener("click", function() {
+  //   var svg =
+  //   `<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+  //     <g transform="translate(-200.000000, -60.000000)" fill="#FFFFFF">
+  //       <g transform="translate(200.000000, 60.000000)">
+  //         <path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g>
+  //   </svg>`;
+  //   var encoded = window.btoa(svg);
+  //   box1.style.background = "url(data:image/svg+xml;base64," + encoded + ")";
+  // });
 
 
 }());
